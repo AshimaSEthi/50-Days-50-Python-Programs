c22932694219dbf0003ff0cddf71fe3b24869a6d
Ashima Sethi <31921103+AshimaSEthi@users.noreply.github.com>
2021-08-14 09:28:46 +0530

    How to Convert Kilometers to Miles in Python

---
+++ program7.py
@@ -0,0 +1,7 @@
+#To convert Kilometer to Miles
+# 1 Km = 0.621371 miles
+input_data = input("Enter the number of Kilometers To Convert: ")
+
+result = int(input_data)*0.621371
+
+print("The required output is: ",result)
