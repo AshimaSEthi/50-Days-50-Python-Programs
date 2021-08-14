5645ca7d6bf594f063cfa844f1b1f62ddb2c3001
Ashima Sethi <31921103+AshimaSEthi@users.noreply.github.com>
2021-08-14 09:25:50 +0530

    How to Add 2 Numbers in Python

---
+++ pragram2.py
@@ -0,0 +1,13 @@
+# How to Add 2 numbers in Python
+
+num1 = 45
+num2 = 78
+
+add_numbers = num1+num2
+#print(add_numbers)
+
+num3 = 'coding '
+num4 = 'mente'
+
+add_strings = num3+num4
+print(add_strings)
