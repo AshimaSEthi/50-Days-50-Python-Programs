f39493440aa78df2eb5b44931ec4efd2010d162d
Ashima Sethi <31921103+AshimaSEthi@users.noreply.github.com>
2021-08-14 09:29:33 +0530

    How to check if No. is even or odd in Python

---
+++ program9.py
@@ -0,0 +1,7 @@
+#To check if num entered is even or odd
+input_entered = int(input("Enter the Number: "))
+
+if input_entered %2 ==0:
+	print("It is an Even Number")
+else:
+	print("It is an Odd Number")
