cc2091c2e5be2604b4140c643c0dd8f6aecab810
Ashima Sethi <31921103+AshimaSEthi@users.noreply.github.com>
2021-08-14 09:27:54 +0530

    How to Swap Two Numbers in Python

---
+++ program5.py
@@ -0,0 +1,15 @@
+#Swapping of Two numbers
+
+num1 =4
+num2 = 5
+
+print('The value of num1 = ',num1)
+print('The value of num2 = ',num2)
+
+#how to variable assignment works in python
+temp = num1
+num1 = num2
+num2 = temp
+
+print('The value of num1 = ',num1)
+print('The value of num2 = ',num2)
